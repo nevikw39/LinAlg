88e6b056b1730b0100c605874fcd6e1da0f116f2
nevikw39 <nevikw39@lgn302.nchc-2020-hpc>
2022-10-16 17:08:23 +0800

    Linear Algebra Assignment 1: run several times

--- A1/A1_110062219.py
+++ A1/A1_110062219.py
@@ -12,25 +12,28 @@ import scipy.linalg.blas as blas
 np.show_config()
 sp.show_config()
 
-rows = [['n', "Time [s]"]]
+rows = []
 
 for i in range(20):
     n = (i + 1) * 1000
+    s = 0
 
-    a = np.random.rand(n, n)
-    b = np.random.rand(n, n)
+    for j in range(10):
+        a = np.random.rand(n, n)
+        b = np.random.rand(n, n)
 
-    tik = time.perf_counter()
-    c = blas.dgemm(1, a, b)
-    tok = time.perf_counter()
+        tik = time.perf_counter()
+        c = blas.dgemm(1, a, b)
+        tok = time.perf_counter()
 
-    d = np.dot(a, b)
-    assert np.allclose(c, d)
+        d = np.dot(a, b)
+        assert np.allclose(c, d)
 
-    lst = [n, tok - tik]
+        s += tok - tik
+
+    lst = [n, s / 10]
     rows.append(lst)
     print(lst)
 
 with open("plot.csv", 'w') as f:
     csv.writer(f).writerows(rows)
-
